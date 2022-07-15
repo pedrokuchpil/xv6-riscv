c4a56ea6936607f1938ff47f0053208539f5e38b
pedrokuchpil <pedrokuchpil@gmail.com>
2022-07-15 14:49:59 -0300

    Add trace system call

--- kernel/proc.c
+++ kernel/proc.c
@@ -295,6 +295,9 @@ fork(void)
   // Cause fork to return 0 in the child.
   np->trapframe->a0 = 0;
 
+  //Copy tracemask from parent
+  np->tracemask = p->tracemask;
+
   // increment reference counts on open file descriptors.
   for(i = 0; i < NOFILE; i++)
     if(p->ofile[i])
